8d553bdbf9a3dc4875d87e5eb3cde6a8bc457126
Souza <lucas11souza97@gmail.com>
2025-02-02 14:06:05 -0400

    usando apis exteras no chatbot

--- chatbot/app/config.py
+++ chatbot/app/config.py
@@ -5,3 +5,4 @@ load_dotenv()
 
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
--- chatbot/app/run.py
+++ chatbot/app/run.py
@@ -1,23 +1,17 @@
 from litellm import completion
+import requests
+import json
+from app.config import GROQ_API_KEY, WEATHER_API_KEY
 
-from app.config import GROQ_API_KEY
-
-
-messages = [
-    {"role": "system", "content": "Você é o chat da Terra e do Universo, que responde perguntas em português brasileiro sobre previsão do tempo na Terra e no espaço próximo à Terra, além de informações sobre terremotos."},
-    {"role": "user", "content": "Qual é a frequência dos máximos solares?"}
-]
-
-
-def call_groq_api(messages: list, model="groq/llama-3.3-70b-versatile"):
-  response = completion(
-        model=model,
-        messages=messages,
-        api_key=GROQ_API_KEY,
-    )
-  resposta = response.choices[0].message.content
-#   print(resposta)
-  return resposta
+# def call_groq_api(messages: list, model="groq/llama-3.3-70b-versatile"):
+#   response = completion(
+#         model=model,
+#         messages=messages,
+#         api_key=GROQ_API_KEY,
+#     )
+#   resposta = response.choices[0].message.content
+# #   print(resposta)
+#   return resposta
 
 
 def chat():
@@ -35,6 +29,106 @@ def chat():
         model_response = call_groq_api(messages)
         messages.append({"role": "assistant", "content": model_response})
         print(f"Assistente: {model_response}")
+
+
+def previsao_do_tempo(city: str, country: str):
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city},{country}&APPID={WEATHER_API_KEY}&lang=pt_br&units=metric'
+    response = requests.get(url)
+    data = response.json()
+        
+    return json.dumps(data, indent=4)
+
+# print(WEATHER_API_KEY)
+
+# data = previsao_do_tempo("São Paulo", "BR")
+# print(data)
+# chat()
+
+def verificar_tempestade_solar():
+    url = "https://services.swpc.noaa.gov/products/noaa-planetary-k-index.json"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        latest_kp = float(data[-1][1])  # O último valor Kp
+        if latest_kp >= 5:
+            return f"Alerta de tempestade solar! Índice Kp atual: {latest_kp}"
+        else:
+            return f"Sem tempestade solar no momento. Índice Kp atual: {latest_kp}"
+    else:
+        return "Não foi possível obter informações sobre tempestades solares no momento."
+
+tools = [{
+    "type": "function",
+    "function": {
+        "name": "previsao_do_tempo",
+        "description": "Retorna a previsão do tempo em uma cidade e país",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "city": {
+                    "type": "string",
+                    "description": "Nome da cidade"
+                },
+                "country": {
+                    "type": "string",
+                    "description": "Sigla do país"
+                }
+            },
+            "required": ["city", "country"]
+        }
+    }
+    
+},
+{
+"type": "function",
+"function": {
+    "name": "verificar_tempestade_solar",
+    "description": "Verifica se há uma tempestade solar em andamento",
+    "parameters": {
+        "type": "object",
+        "properties": {},
+        "required": [],
+    },
+}
+
+}]
+
+# Função para chamar a API com o histórico de mensagens
+def call_groq_api(messages: list[dict[str, str]], model="groq/llama-3.3-70b-versatile"):
+    global tools
+    response = completion(
+        model=model,
+        messages=messages,
+        tools=tools,
+        tool_choice="auto",
+        api_key=GROQ_API_KEY,
+    )
+    resposta_texto = response.choices[0].message
+    chamada_ferramentas = resposta_texto.tool_calls
+    if chamada_ferramentas:
+      available_functions = {
+        "previsao_do_tempo": previsao_do_tempo,
+        'verificar_tempestade_solar': verificar_tempestade_solar
+      }
+      for tool_call in chamada_ferramentas:
+        function_name = tool_call.function.name
+        function_to_call = available_functions[function_name]
+        function_args = json.loads(tool_call.function.arguments)
+
+        match function_name:
+          case 'previsao_do_tempo':
+            function_response = function_to_call(
+            city=function_args.get("city"),
+            country=function_args.get("country"),
+          )
+          case "verificar_tempestade_solar":
+            function_response = function_to_call()
+
+        return function_response
+
+    else:
+      return resposta_texto.content
+    
 
 
 chat()
